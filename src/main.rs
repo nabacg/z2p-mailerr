1c00c0773cdc51bc4cbae9542e2bf52498d729d3
Grzegorz Caban <nabacg@gmail.com>
2023-06-01 23:06:06 +0100

    /health_check + integration test for it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use z2p_mailerr::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await 
 }
---
+++ tests/healh_check.rs
@@ -0,0 +1,30 @@
+use std::{net::TcpListener, error::Error};
+
+
+// `cargo expand --test health_check`
+#[tokio::test]
+async fn health_check_succeeds() {
+    // start web server 
+    let local_addrs = spawn_app().expect("failed to spawn App");
+
+    let client = reqwest::Client::new();
+    let response =  client
+        .get(format!("{}/health_check", local_addrs))
+        .send()
+        .await
+        .expect("Failed to execute GET  request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> Result<String, Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+
+
+    let socket_addr = listener.local_addr().map_err(|e| e.into());
+    tokio::spawn(z2p_mailerr::run(listener)?);
+
+    
+    socket_addr.map(|p| format!("http://127.0.0.1:{}", p.port()))
+}
