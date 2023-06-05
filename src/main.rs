72c0fba51f2b44f65a9d257009bfbde273978dc8
Grzegorz Caban <nabacg@gmail.com>
2023-06-06 00:15:21 +0100

    switching log crate to tracing crate, plus structured json logging

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use z2p_mailerr::{configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // set Logger, if RUST_LOG is not set, default to `info`
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect all `log` calls to tracing subscriber below
+    LogTracer::init().expect("Failed to set logger");
+
+    // set trace span level using RUST_LOG env var and  default to `info` if not set
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "z2p_mailerr".into(),
+        // output formatted spans to STDOUT
+        std::io::stdout,
+    );
+
+    //setup subscriber
+    // `.with` provided layer::SubscriberExt, an extension trait
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set global tracing subscriber");
 
     let config_settings =
         configuration::get_configuration().expect("failed to read configuration!");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,12 +14,17 @@ pub async fn subscribe(
     form: web::Form<SubscriptionForm>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
-    log::info!(
-        "Adding '{}' '{}' as a new subscriber",
-        form.email,
-        form.name
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name  = %form.name
     );
-    log::info!("Saving new subscriber details in DB");
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in DB");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,14 +36,15 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber saved in DB.");
+            tracing::info!("New subscriber saved in DB.");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute INSERT INTO subscriptions query: {:?}", e);
+            tracing::error!("Failed to execute INSERT INTO subscriptions query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
