944489b3bad7d9e2bb53827cb575167a4f11bb9a
Grzegorz Caban <nabacg@gmail.com>
2023-06-05 23:08:46 +0100

    adding logging with log and env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use z2p_mailerr::{configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // set Logger, if RUST_LOG is not set, default to `info`
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config_settings =
         configuration::get_configuration().expect("failed to read configuration!");
     let connection_url = config_settings.database.connection_string();
