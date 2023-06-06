59385df59c1b45a5c803dba8f0703c82617c8be9
Grzegorz Caban <nabacg@gmail.com>
2023-06-06 23:35:22 +0100

    fixing clippy

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
     let config_settings =
         configuration::get_configuration().expect("failed to read configuration!");
     let connection_url = config_settings.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_url.expose_secret())
+    let connection_pool = PgPool::connect(connection_url.expose_secret())
         .await
         .expect("failed to connect to Database!");
 
